0bdfe475731b7e223dea6829d1f02e4c18d774e8
LeebSite <mhd.ghalibpradipa@gmail,com>
2024-04-27 19:35:51 +0700

    second commit

--- app/src/main/java/com/example/githubuserapp/data/retrofit/ApiService.java
+++ app/src/main/java/com/example/githubuserapp/data/retrofit/ApiService.java
@@ -11,18 +11,18 @@ import retrofit2.http.Query;
 
 public interface ApiService {
 
-    @Headers({"Authorization: token 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"})
+    @Headers({"Authorization: token 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"})
     @GET("search/users")
     Call<GithubSearchResponse> searchUsers(@Query("q") String query);
 
-    @Headers({"Authorization: token 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"})
+    @Headers({"Authorization: token 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"})
     @GET("users/{username}")
     Call<User> getUser(@Path("username") String username);
-    @Headers({"Authorization: token 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"})
+    @Headers({"Authorization: token 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"})
     @GET("users/{username}/followers")
     Call<User> getFollowers(@Path("username") String username);
 
-    @Headers({"Authorization: token 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"})
+    @Headers({"Authorization: token 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"})
     @GET("users/{username}/following")
     Call<User> getFollowing(@Path("username") String username);
 
